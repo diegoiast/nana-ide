5b92cc8d5210f5090c6538a418ec3b70d3f65a27
Diego Iastrubni <diegoiast@gmail.com>
2021-01-24 00:02:25 +0200

    Support for building
    This is a very hack to just execute "ninja -C build" and then
    reading the output. This effectivly builds "a" project.
    
    It was way too easy...

--- main.cpp
+++ main.cpp
@@ -8,12 +8,12 @@
 #include <nana/gui/widgets/panel.hpp>
 #include <nana/gui/widgets/tabbar.hpp>
 #include <nana/gui/widgets/toolbar.hpp>
-
+#include <nana/threads/pool.hpp>
 
 #include <memory>
 #include <vector>
 #include <map>
-
+#include <functional>
 
 class tab_page_editor : public nana::panel<false>
 {
@@ -34,8 +34,33 @@ public:
         }
 };
 
+#ifdef __linux__
+int exec(const char* cmd, std::function<void(const char*)> on_line_read)
+{
+        char line[1024];
+        FILE* pipe = popen(cmd, "r");
+        if (!pipe) {
+                return -1;
+        }
+        while (fgets(line, sizeof line, pipe) != NULL) {
+                on_line_read(line);
+        }
+        pclose(pipe);
+        return 0;
+}
+#endif
+
+#ifdef __WIN32__
+// https://stackoverflow.com/a/46348112/78712
+int exec(const char* cmd, std::function<void(const char*)> on_line_read) {
+{
+        // TODO
+}
+#endif
+
 int main() {
-        nana::form fm;
+        nana::threads::pool thread_pool;
+        nana::form fm{};
 
         nana::place plc{fm};
 
@@ -47,31 +72,51 @@ int main() {
 
         tab_page_editor editor1(fm);
         tab_page_editor editor2(fm);
-        nana::label lbl(fm);
+        nana::textbox lbl(fm);
+        lbl.editable(false);
 
         nana::tabbar<std::string> tabs(fm);
         tabs.append("main.cpp", editor1);
         tabs.append("CMakeLists.txt", editor2);
 
         nana::toolbar tb(fm);
+        tb.separate();
         tb.append("Open");
         tb.append("Compile");
         tb.append("run");
 
+        nana::button buttonOpen{fm, "Open"};
+        nana::button buttonCompile{fm, "Compile"};
+        nana::button buttonRun{fm, "Run"};
+
+        buttonOpen.events().click([](){
+        });
+        buttonCompile.events().click(nana::threads::pool_push(thread_pool, [&lbl, &buttonCompile, &fm]{
+                buttonCompile.enabled(false);
+                buttonCompile.caption("Compiling...");
+                lbl.caption("");
+                exec("ninja -C build", [&lbl, &fm](const char* line){
+                        lbl.append(line, false);
+                });
+                buttonCompile.caption("Compile");
+                buttonCompile.enabled(true);
+        }));
+        buttonRun.events().click([](){
+        });
+
         plc.div("vertical "
-                        "<toolbar weight=32>"
+                        "<toolbar weight=32 gap=5 arrange=[100,repeated]>"
                         "<<list weight=20%>|<vert <tabs weight=32><tab_frame>|<panel weight=20%>>>"
-                        "<status weight=32>"
                 "");
-        plc["toolbar"] << tb;
+        plc["toolbar"] << buttonOpen << buttonCompile << buttonRun;
         plc["list"] << lsbox;
         plc["tabs"] << tabs;
         plc["tab_frame"].fasten(editor1).fasten(editor2);
-        plc["panel"] << lbl.caption("testing");
+        plc["panel"] << lbl;
         plc.collocate();
 
         fm.show();
+        nana::API::zoom_window(fm, true);
         nana::exec();
         return 0;
 }
-
