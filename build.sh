9c0e2f37b664e7ee0ac3ba76be016707c882c2e9
Diego Iastrubni <diegoiast@gmail.com>
2021-01-25 11:55:41 +0200

    Build system upgrades
     - ops, I was not using connan properly. Now it works :)
     - build.sh has support for Makefiles, and parralelism on that case.
     - better documentation in CMakeFile.txt

--- build.sh
+++ build.sh
@@ -9,12 +9,13 @@ export PATH=$PATH:"$HOME/.local/bin/"
 CC="ccache clang"
 CXX="ccache clang++"
 CMAKE_GENERATOR="Ninja"
+#CMAKE_GENERATOR="Unix Makefiles"
 BUILD_DIR="cbuild"
 
 rm -fr $BUILD_DIR
 mkdir $BUILD_DIR
 cd $BUILD_DIR
 conan install ../
-cmake .. -G $CMAKE_GENERATOR
+cmake .. -G "$CMAKE_GENERATOR"
 cd ..
-cmake --build $BUILD_DIR
+cmake --build $BUILD_DIR --parallel
